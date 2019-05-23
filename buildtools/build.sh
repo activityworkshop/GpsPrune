2302358503c38817e19f6e529f6c9e530aac0e86
activityworkshop <mail@activityworkshop.net>
2019-05-23 22:02:03 +0200

    Update build scripts

--- buildtools/build.sh
+++ buildtools/build.sh
@@ -7,16 +7,19 @@ rm -rf compile
 rm -rf dist
 # create compile directory
 mkdir compile
+echo "building..."
 # compile java
-javac -d compile $( find tim -name "*.java" -print )
+# TODO: If your java3d libraries are not under /usr/share/java, please edit the following line with the correct path
+javac -d compile -cp /usr/share/java/vecmath.jar:/usr/share/java/j3dutils.jar:/usr/share/java/j3dcore.jar $( find src -name "*.java" -print )
 # add other required resources
-cp -r tim/prune/lang compile/tim/prune/
-cp -r tim/prune/*.txt compile/tim/prune/
-cp -r tim/prune/gui/images compile/tim/prune/gui/
-cp tim/prune/function/srtm/srtmtiles.dat compile/tim/prune/function/srtm
+cp -r src/tim/prune/lang compile/tim/prune/
+cp -r src/tim/prune/*.txt compile/tim/prune/
+cp -r src/tim/prune/gui/images compile/tim/prune/gui/
+cp src/tim/prune/function/srtm/srtmtiles.dat compile/tim/prune/function/srtm
 # make dist directory
 mkdir dist
 # build into jar file
-jar cfm dist/${PRUNENAME}.jar MANIFEST.MF -C compile .
+jar cfm dist/${PRUNENAME}.jar buildtools/MANIFEST.MF -C compile .
 # finished!
 echo "build complete"
+
