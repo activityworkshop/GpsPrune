b0d2a142da5feaa4eed706558fdf5b7902642792
activityworkshop <mail@activityworkshop.net>
2021-01-10 11:52:20 +0100

    Add Swedish language for 20.2

--- src/tim/prune/GpsPrune.java
+++ src/tim/prune/GpsPrune.java
@@ -38,9 +38,9 @@ import tim.prune.gui.profile.ProfileChart;
 public class GpsPrune
 {
 	/** Version number of application, used in about screen and for version check */
-	public static final String VERSION_NUMBER = "20.1";
+	public static final String VERSION_NUMBER = "20.2";
 	/** Build number, just used for about screen */
-	public static final String BUILD_NUMBER = "382";
+	public static final String BUILD_NUMBER = "383";
 	/** Static reference to App object */
 	private static App APP = null;
 
