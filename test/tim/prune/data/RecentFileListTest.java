aa0c89928d8963800d15bb1767707aace424f5ff
Rob <githubpages@s..........com>
2023-07-31 17:36:31 +0800

    update imports to use junit5 assertions instead of 4

--- test/tim/prune/data/RecentFileListTest.java
+++ test/tim/prune/data/RecentFileListTest.java
@@ -1,9 +1,9 @@
 package tim.prune.data;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.io.File;
 import java.io.IOException;
--- test/tim/prune/data/RecentFileTest.java
+++ test/tim/prune/data/RecentFileTest.java
@@ -1,8 +1,8 @@
 package tim.prune.data;
 
-import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import java.io.File;
 import java.io.IOException;
