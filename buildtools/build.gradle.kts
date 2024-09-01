80120c6d5386ae44d35abf4107f601fde0357d98
activityworkshop <mail@activityworkshop.net>
2024-09-01 16:22:52 +0200

    Translations and versioning to 24.4

--- buildtools/build.gradle.kts
+++ buildtools/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "tim.prune"
-version = 24.3
+version = 24.4
 
 java {
     sourceCompatibility = JavaVersion.VERSION_11
--- src/tim/prune/GpsPrune.java
+++ src/tim/prune/GpsPrune.java
@@ -42,9 +42,9 @@ import tim.prune.gui.profile.ProfileChart;
 public class GpsPrune
 {
 	/** Version number of application, used in about screen and for version check */
-	public static final String VERSION_NUMBER = "24.3";
+	public static final String VERSION_NUMBER = "24.4";
 	/** Build number, just used for about screen */
-	public static final String BUILD_NUMBER = "417";
+	public static final String BUILD_NUMBER = "419";
 	/** Static reference to App object */
 	private static App APP = null;
 
