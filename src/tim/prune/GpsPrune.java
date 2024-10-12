ea70901c57d4a416463060b230ae675c75852dd8
activityworkshop <mail@activityworkshop.net>
2024-10-12 12:24:43 +0200

    Translations and versioning to 24.5

--- src/tim/prune/GpsPrune.java
+++ src/tim/prune/GpsPrune.java
@@ -42,9 +42,9 @@ import tim.prune.gui.profile.ProfileChart;
 public class GpsPrune
 {
 	/** Version number of application, used in about screen and for version check */
-	public static final String VERSION_NUMBER = "24.4";
+	public static final String VERSION_NUMBER = "24.5";
 	/** Build number, just used for about screen */
-	public static final String BUILD_NUMBER = "419";
+	public static final String BUILD_NUMBER = "421";
 	/** Static reference to App object */
 	private static App APP = null;
 
