64222ab7937fc907eb2ba2272ba79afaa4e357d3
activityworkshop <mail@activityworkshop.net>
2025-03-05 18:50:25 +0100

    Fix sorting of dates in segment comparison table (Issue 110)

--- src/tim/prune/function/comparesegments/SegmentTableModel.java
+++ src/tim/prune/function/comparesegments/SegmentTableModel.java
@@ -31,7 +31,7 @@ public class SegmentTableModel extends AbstractTableModel
 		private final Object[] _values = new Object[NUM_COLUMNS];
 		private TableValues(SegmentSummary inSegment, TimeZone inTimezone, Unit inDistUnits)
 		{
-			_values[0] = inSegment.getStartTimestamp().getDateText(inTimezone);
+			_values[0] = new DateForTable(inSegment.getStartTimestamp(), inTimezone);
 			_values[1] = inSegment.getStartTimestamp().getTimeText(inTimezone);
 			_values[2] = Distance.convertRadiansToDistance(inSegment.getDistanceInRadians(), inDistUnits);
 			_values[3] = new Duration(inSegment.getDurationInSeconds());
@@ -40,6 +40,8 @@ public class SegmentTableModel extends AbstractTableModel
 			return _values[inColumnIndex];
 		}
 	}
+
+
 
 	/**
 	 * Initialize the table model with the segment list
@@ -76,6 +78,9 @@ public class SegmentTableModel extends AbstractTableModel
 	 */
 	public Class<?> getColumnClass(int inColumnIndex)
 	{
+		if (inColumnIndex == 0) {
+			return DateForTable.class;
+		}
 		if (inColumnIndex == 2) {
 			return Double.class;
 		}
